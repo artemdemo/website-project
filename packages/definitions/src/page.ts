b5310fd78a726ca392f538580de3436bbeb58e6b
Artem Demo <artemdemo@gmail.com>
2025-01-24 20:28:13 +0200

    updated type fixes leftovers

--- packages/definitions/src/page.ts
+++ packages/definitions/src/page.ts
@@ -3,8 +3,8 @@ import { z } from 'zod';
 export const pageConfigSchema = z.object({
   title: z.string(),
   date: z.string(),
-  featuredImage: z.string(),
-  tags: z.array(z.string()),
+  featuredImage: z.string().optional(),
+  tags: z.array(z.string()).optional(),
 });
 
 export type PageConfig = z.infer<typeof pageConfigSchema>;
