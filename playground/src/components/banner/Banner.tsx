5e03fb94ad279596107288aaf833fe37decb9cc4
Artem Demo <artemdemo@gmail.com>
2025-02-08 09:41:27 +0200

    added banner

---
+++ playground/src/components/banner/Banner.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Banner.css';
+
+export const Banner: React.FC = () => {
+  return (
+    <div className='Banner'>
+      This is a banner
+    </div>
+  );
+};
--- playground/src/pages/index.tsx
+++ playground/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { PageComponent } from 'site-builder/types';
 import { TopMenu } from '../components/menu/TopMenu';
+import { Banner } from '../components/banner/Banner';
 
 const Posts: PageComponent = () => {
   useEffect(() => {
@@ -8,6 +9,7 @@ const Posts: PageComponent = () => {
   }, []);
   return (
     <>
+      <Banner />
       <TopMenu />
       <h1>Posts List</h1>
     </>
