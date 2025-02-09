b6d24fed63f0307b863f47ce2d10ee5391de2035
Artem Demo <artemdemo@gmail.com>
2025-02-09 21:20:20 +0200

    e2e tests

---
+++ packages/e2e-tests/src/build.spec.ts
@@ -0,0 +1,8 @@
+import { describe, it } from 'vitest';
+import { e2eTestkit } from './infra/e2eTestkit';
+
+describe('build', () => {
+  const { driver, builders } = e2eTestkit();
+
+  it('should', async () => {});
+});
---
+++ packages/e2e-tests/src/infra/e2eTestkit.ts
@@ -0,0 +1,9 @@
+import { createBuilders } from './builders/createBuilders';
+import { createDriver } from './drivers/createDriver';
+
+export const e2eTestkit = () => {
+  return {
+    driver: createDriver(),
+    builders: createBuilders(),
+  }
+};
