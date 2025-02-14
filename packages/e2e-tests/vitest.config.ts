f1e83af62d7d979d9f0f9c78b988921607a80107
Artem Demo <artemdemo@gmail.com>
2025-02-14 20:10:14 +0200

    vitest config

--- packages/e2e-tests/vitest.config.ts
+++ packages/e2e-tests/vitest.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ['src/**/*.{test,spec}.ts'],
+    include: ['src/*.{test,spec}.ts'],
     environment: 'node',
     exclude: ['build', 'node_modules', 'dist', '.turbo'],
     maxConcurrency: 7,
+    // Setup could take some time, so I'm increasing default timeout
+    testTimeout: 10000,
   },
 });
