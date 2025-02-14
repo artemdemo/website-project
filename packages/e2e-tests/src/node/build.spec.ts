c993906643731ec06702b9d5548b35853d939fb5
Artem Demo <artemdemo@gmail.com>
2025-02-14 14:33:50 +0200

    node tests

--- packages/e2e-tests/src/node/build.spec.ts
+++ packages/e2e-tests/src/node/build.spec.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from 'vitest';
 import { existsSync } from 'node:fs';
 import { readFile } from 'node:fs/promises';
-import { testkit } from './infra/testkit';
+import { testkit } from '../infra/testkit';
 import { join } from 'node:path';
 
 describe('Build Command', () => {
--- packages/e2e-tests/vitest.config.ts
+++ packages/e2e-tests/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ['**/*.{test,spec}.ts?(x)'],
+    include: ['src/node/**/*.{test,spec}.ts'],
     environment: 'node',
     exclude: ['build', 'node_modules', 'dist', '.turbo'],
     maxConcurrency: 7,
